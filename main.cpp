6f91796dc8092a9ebc23d6440b107c6ea31ad79c
jxgonz <johangonzalez7373@gmail.com>
2022-05-12 11:07:51 -0700

    div class implementation and testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 #include "base.hpp"
+#include "op.hpp"
+#include "pow.hpp"
+#include "div.hpp"
 
 int main() {
     // This is a very basic main, and being able to correctly execute this main
@@ -10,10 +13,22 @@ int main() {
     Base* seven = new Op(7);
     Base* four = new Op(4);
     Base* two = new Op(2);
-    Base* mult = new Mult(seven, four);
-    Base* add = new Add(three, mult);
-    Base* minus = new Sub(add, two);
 
-    std::cout << minus->stringify() << " = " << minus->evaluate() << std::endl;
+    //Base* mult = new Mult(seven, four);
+    Base* div = new Div(four, two);
+    //Base* add = new Add(three, mult);
+    //Base* minus = new Sub(add, two);
+    Base* power = new Pow(four, two);
+
+    std::cout << div->stringify() << " = " << div->evaluate() << std::endl;
+    std::cout << power->stringify() << " = " << power->evaluate() << std::endl;
+
+    delete three;
+    delete seven;
+    delete four;
+    delete two;
+    delete div;
+    delete power;
+
     return 0;
 }
